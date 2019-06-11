5e3ed46434b02ccea897ee6be8a8d30d15fd4070
spring <yingchun.lou@wm-motor.com>
2019-06-12 07:06:35 +0800

    解决请求成功但还是 返回404的问题

--- src/main/java/com/imooc/controller/PayController.java
+++ src/main/java/com/imooc/controller/PayController.java
@@ -1,11 +1,13 @@
 package com.imooc.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.imooc.dto.OrderDTO;
 import com.imooc.enums.ResultEnum;
 import com.imooc.exception.SellException;
 import com.imooc.service.OrderService;
 import com.imooc.service.PayService;
 import com.lly835.bestpay.model.PayResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +22,7 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/pay")
+@Slf4j
 public class PayController {
 
     @Autowired
@@ -29,7 +32,27 @@ public class PayController {
     private PayService payService;
 
     @GetMapping("/create")
-    public ModelAndView create(@RequestParam("orderId") String orderId,
+    @ResponseBody
+    public String create(@RequestParam("orderId") String orderId,
+                               @RequestParam("returnUrl") String returnUrl,
+                               Map<String, Object> map) {
+        //1. 查询订单
+        OrderDTO orderDTO = orderService.findOne(orderId);
+        if (orderDTO == null) {
+            throw new SellException(ResultEnum.ORDER_NOT_EXIST);
+        }
+
+        //2. 发起支付
+        PayResponse payResponse = payService.create(orderDTO);
+
+        map.put("payResponse", payResponse);
+        map.put("returnUrl", returnUrl);//用户创建订单，商户存储订单状态，发起预支付。微信返回预支付信息，吊起用户支付，支付后，微信异步通知。
+        log.info("response={}",JSONObject.toJSONString(map));
+        return JSONObject.toJSONString(map);
+    }
+
+    @GetMapping("/create2")
+    public ModelAndView create2(@RequestParam("orderId") String orderId,
                                @RequestParam("returnUrl") String returnUrl,
                                Map<String, Object> map) {
         //1. 查询订单
